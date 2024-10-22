4c84467f7f33e1a9d9fb75f6531a2948170db47b
Leafoliage <leafoliages@gmail.com>
2024-10-22 19:49:41 +0800

    Add Lab6

---
+++ Lab6/lab06.py
@@ -0,0 +1,101 @@
+from djitellopy import Tello
+from pyimagesearch.pid import PID
+import cv2
+import numpy as np
+import math
+
+def mss(update, max_speed_threshold=30):
+    if update > max_speed_threshold:
+        update = max_speed_threshold
+    elif update < -max_speed_threshold:
+        update = -max_speed_threshold
+
+    return update
+
+def teleop(drone):
+    key = cv2.waitKey(33)
+    self.takeoff()
+    drone.move("forward")
+
+def see(drone, markId):
+    frame_read = drone.get_frame_read()
+
+    dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+    parameters = cv2.aruco.DetectorParameters_create()
+
+    fs = cv2.FileStorage("param-drone.xml", cv2.FILE_STORAGE_READ)
+    intrinsic = fs.getNode("intrinsic").mat()
+    distortion = fs.getNode("distortion").mat()
+
+    z_pid = PID(kP=0.7, kI=0.0001, kD=0.1)
+    y_pid = PID(kP=0.7, kI=0.0001, kD=0.1)
+    x_pid = PID(kP=0.7, kI=0.0001, kD=0.1)
+    yaw_pid = PID(kP=0.7, kI=0.0001, kD=0.1)
+
+    z_pid.initialize()
+    y_pid.initialize()
+    x_pid.initialize()
+    yaw_pid.initialize()
+
+    while True:
+        frame = frame_read.frame
+        markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame, dictionary, parameters=parameters)
+        print(markerIds)
+        
+        frame = cv2.aruco.drawDetectedMarkers(frame, markerCorners, markerIds)
+
+        cv2.imshow('frame', frame)
+        key = cv2.waitKey(33)
+        if key != -1:
+            keyboard(drone, key)
+        elif markerIds is not None:
+            if markId != markerIds[0][0]:
+                continue
+            rvec, tvec, _objPoints = cv2.aruco.estimatePoseSingleMarkers(markerCorners, 15, intrinsic, distortion)
+            (x_err, y_err, z_err) = tvec[0][0]
+            z_err = z_err - 100
+            x_err = x_err * 2
+            y_err = - y_err * 2
+
+            R, err = cv2.Rodrigues(rvec)
+            # print("err:", err)
+            V = np.matmul(R, [0, 0, 1])
+            rad = math.atan(V[0]/V[2])
+            deg = rad / math.pi * 180
+            print(deg)
+            yaw_err = yaw_pid.update(deg, sleep=0)
+            
+            x_err = mss(x_err)
+            y_err = mss(y_err)
+            z_err = mss(z_err)
+            yaw_err = mss(yaw_err)
+
+            print("errs:", x_err, y_err, z_err, yaw_err)
+            
+            xv = x_pid.update(x_err, sleep=0)
+            yv = y_pid.update(y_err, sleep=0)
+            zv = z_pid.update(z_err, sleep=0)
+            rv = yaw_pid.update(yaw_err, sleep=0)
+            # print(xv, yv, zv, rv)
+            # drone.send_rc_control(min(20, int(xv//2)), min(20, int(zv//2)), min(20, int(yv//2)), 0)
+            if abs(z_err) <= 30 and abs(y_err) <= 20 and abs(yaw_err) <= 10:
+                return
+            else: 
+                continue
+                # drone.send_rc_control(0, int(zv//2), int(yv), int(yaw_err))
+        else:
+            drone.send_rc_control(0, 0, 0, 0)
+
+
+if __name__ == '__main__':
+    drone = Tello()
+    drone.connect()
+    drone.streamon()
+
+    frame_read = drone.get_frame_read()
+    # calibration(frame_read)
+
+    # teleop(drone)
+    see(drone, 1)
+    print("done")
+    # auto(drone)
